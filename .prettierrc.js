c04414c2c340b9a9f587eb67f8c3022544bf23ae
inorganik <jamie.perkins@gmail.com>
2025-10-20 09:08:38 -0600

    prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	bracketSpacing: true,
+	semi: true,
+	quoteProps: 'as-needed',
+	singleQuote: true,
+	arrowParens: 'avoid',
+	endOfLine: 'auto',
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderParserPlugins: ['typescript', 'decorators-legacy'],
+};
