287f155b8e3604db26c3c29db24be0d1e60573c2
inorganik <jamie.perkins@gmail.com>
2025-10-20 09:35:26 -0600

    load schema

--- src/app/enablement/enablement.component.ts
+++ src/app/enablement/enablement.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-enablement',
-  imports: [],
-  templateUrl: './enablement.component.html',
-  styleUrl: './enablement.component.scss'
+	selector: 'app-enablement',
+	imports: [],
+	templateUrl: './enablement.component.html',
+	styleUrl: './enablement.component.scss',
 })
-export class EnablementComponent {
+export class EnablementComponent implements OnInit {
+	private http = inject(HttpClient);
+	schema: any;
 
+	ngOnInit() {
+		this.http.get('/enablement-schema.json').subscribe(schema => {
+			this.schema = schema;
+			console.log('Schema loaded:', schema);
+		});
+	}
 }
